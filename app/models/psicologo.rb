d369e352cd2cf58e5542a64965572abd22b4d871
Rodrigo César <rodrigocnn@gmail.com>
2025-08-20 19:23:29 -0300

    feat(psicologo): adiciona autenticação com has_secure_password e validações básicas

--- app/models/psicologo.rb
+++ app/models/psicologo.rb
@@ -1,2 +1,15 @@
 class Psicologo < ApplicationRecord
+  has_secure_password
+
+  # Validações básicas
+  validates :nome, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+
+private
+
+  def password_required?
+    password_digest.blank? || !password.nil?
+  end
 end
