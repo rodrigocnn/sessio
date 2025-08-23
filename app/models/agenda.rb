42c59405c891d4a305661ef3dc216db468259dd7
Rodrigo César <rodrigocnn@gmail.com>
2025-08-23 11:10:02 -0300

    feat(model-agenda): adiciona validação do horario da agenda

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,4 +1,13 @@
 class Agenda < ApplicationRecord
   belongs_to :psicologo
   belongs_to :paciente
+
+
+  validate :start_must_be_before_end
+
+  def start_must_be_before_end
+    if start.present? && self.end.present? && start >= self.end
+      errors.add(:start, "Horário inicial não pode ser maior ou igual ao horário de término")
+    end
+  end
 end
