67dace7ff3d7d6b42063311c66b7137aa773055b
Rodrigo César <rodrigocnn@gmail.com>
2025-08-23 11:33:30 -0300

    feat(model-pacientes): adiciona validações

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,3 +1,8 @@
 class Paciente < ApplicationRecord
   belongs_to :psicologo
+
+  validates :nome, presence: true
+  validates :email, presence: true
+  validates :telefone, presence: true
+  validates :psicologo_id, presence: true
 end
