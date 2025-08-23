8f1ca7245416cc666ee46bd21d85e5fe638e0b69
Rodrigo César <rodrigocnn@gmail.com>
2025-08-23 11:33:55 -0300

    feat(model-psicologo): adiciona validações dos campos crp e especialidade

--- app/models/psicologo.rb
+++ app/models/psicologo.rb
@@ -5,6 +5,8 @@ class Psicologo < ApplicationRecord
   validates :nome, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, if: :password_invalid?
+  validates :crp, presence: true
+  validates :especialidade, presence: true
 
 private
 
