5099d8eb854018f4bfa105a56d9f383bac2c7ddf
Rodrigo César <rodrigocnn@gmail.com>
2025-08-23 12:07:50 -0300

    test(paciente):  implementa teste do model paciente

--- spec/models/paciente_spec.rb
+++ spec/models/paciente_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Paciente, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "should create a paciente with all  attributes" do
+    paciente = create(:paciente)
+    expect(paciente).to be_valid
+  end
+
+    context "Validations" do
+      it "does not allow blank name" do
+        paciente = build(:paciente, nome: "")
+        expect(paciente).to_not be_valid
+      end
+
+      it "does not allow blank email" do
+        paciente = build(:paciente, email: "")
+        expect(paciente).to_not be_valid
+      end
+
+      it "does not allow blank telefone" do
+        paciente = build(:paciente, telefone: "")
+        expect(paciente).to_not be_valid
+      end
+
+      it "does not allow blank data de nascimento" do
+        paciente = build(:paciente, data_nascimento: "")
+        expect(paciente).to_not be_valid
+      end
+
+      it "does not allow blank psicologo" do
+        paciente = build(:paciente, psicologo_id: "")
+        expect(paciente).to_not be_valid
+      end
+    end
 end
